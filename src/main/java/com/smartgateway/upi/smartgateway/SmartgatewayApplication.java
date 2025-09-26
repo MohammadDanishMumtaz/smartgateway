8d325073b0eeffc3ae7803740029618b5ac92ab5
MohammadDanishMumtaz <mumtaz.m.danish@gmail.com>
2025-09-26 23:00:56 +0530

    [User Story-0001] Basic project structure. 20250926

---
+++ src/main/java/com/smartgateway/upi/smartgateway/SmartgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.smartgateway.upi.smartgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartgateway/upi/smartgateway/SmartgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgateway.upi.smartgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
